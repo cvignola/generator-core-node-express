96d83b1d5e1d626c9951512d861fa169c5fde868
Chris Vignola <cvignola@us.ibm.com>
2017-09-06 13:14:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# generator-core-node-express
+
+| master           | development  |
+|------------------|--------------|
+|[![Build Status](https://travis.ibm.com/arf/generator-core-node-express.svg?token=ePBWPJTgR2KYCeTsit1a&branch=master)](https://travis.ibm.com/arf/generator-core-node-express)|[![Build Status](https://travis.ibm.com/arf/generator-core-node-express.svg?token=ePBWPJTgR2KYCeTsit1a&branch=development)](https://travis.ibm.com/arf/generator-core-node-express)
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-express using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+```
+If you want to use the generator just install it using NPM
+
+```bash
+npm install -g @arf/generator-core-node-express
+```
+
+For development clone this repository
+```bash
+git clone https://github.ibm.com/arf/generator-core-node-express.git
+```
+
+`npm link` inside the repository directory:
+
+ ```bash
+ npm link
+ ```
+
+Change into a new directory and generate your project with:
+
+```bash
+yo @arf/core-node-express
+```
+
+To test
+
+```bash
+npm test
+```
---
+++ app/index.js
@@ -0,0 +1,83 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const Bundle = require("./../package.json")
+const Log4js = require('log4js');
+const logger = Log4js.getLogger("generator-core-node-express");
+
+const OPTION_BLUEMIX = "bluemix";
+const OPTION_SPEC = "spec";
+
+module.exports = class extends Generator {
+
+  constructor(args, opts) {
+    super(args, opts);
+    logger.info("Package info ::", Bundle.name, Bundle.version);
+
+    // bluemix option for YaaS integration
+    this.argument(OPTION_BLUEMIX, {
+      desc: 'Option for deploying with Bluemix. Stringified JSON.',
+      required: false,
+      hide: true,
+      type: String
+    });
+
+    // spec as json
+    this.argument(OPTION_SPEC, {
+      desc: 'The generator specification. Stringified JSON.',
+      required: false,
+      hide: true,
+      type: String
+    });
+  }
+
+
+  initializing() {
+    this.skipPrompt = true;
+    this._sanitizeOption(this.options, OPTION_BLUEMIX);
+    this._sanitizeOption(this.options, OPTION_SPEC);
+  }
+
+  writing() {
+    this.fs.copyTpl(this.templatePath('server'), this.destinationPath('server'), this.options);
+    this.fs.copyTpl(this.templatePath('test'), this.destinationPath('test'), this.options);
+    this.fs.copyTpl(this.templatePath('_gitignore'), this.destinationPath('.gitignore'), this.options);
+    this.fs.copyTpl(this.templatePath('cli-config.yml'), this.destinationPath('cli-config.yml'), this.options);
+    this.fs.copyTpl(this.templatePath('Dockerfile'), this.destinationPath('Dockerfile'), this.options);
+    this.fs.copyTpl(this.templatePath('Dockerfile-tools'), this.destinationPath('Dockerfile-tools'), this.options);
+    this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.options);
+    this.fs.copyTpl(this.templatePath('_package-lock.json'), this.destinationPath('package-lock.json'), this.options);
+    this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), this.options);
+
+    if(this.options.spec && this.options.spec.applicationType === 'MS') {
+      this.fs.copy(this.templatePath('public/swagger-ui'), this.destinationPath('public/swagger-ui'));
+      this.fs.copyTpl(this.templatePath('public/swagger.yaml'), this.destinationPath('public/swagger.yaml'), this.options);
+      this.fs.delete(this.destinationPath('server/routers/public.js'));
+    } else {
+      this.fs.copy(this.templatePath('public/index.html'), this.destinationPath('public/index.html'));
+      this.fs.delete(this.destinationPath('server/routers/swagger.js'));
+    }
+  }
+
+  _sanitizeOption(options, name) {
+    let optionValue = options[name];
+    if (!optionValue) {
+      return logger.error("Missing", name, "parameter");
+    }
+
+    if (optionValue.indexOf("file:") === 0) {
+      let fileName = optionValue.replace("file:", "");
+      let filePath = this.destinationPath("./" + fileName);
+      logger.info("Reading", name, "parameter from local file", filePath);
+      this.options[name] = this.fs.readJSON(filePath);
+      return;
+    }
+
+    try {
+      this.options[name] = typeof(this.options[name]) === "string" ?
+        JSON.parse(this.options[name]) : this.options[name];
+    } catch (e) {
+      logger.error(e);
+      throw name + " parameter is expected to be a valid stringified JSON object";
+    }
+  }
+};
